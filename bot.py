68a5ae3ecc04e9131359a0501b00ec7205d6e23b
mohsiur <mohsiurr@buffalo.edu>
2017-03-10 20:58:24 -0500

    Integrated firebae

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
-import re, praw, requests, os, sys, urllib
+import re, praw, requests, os, sys, urllib, time, requests, json
+
+firebase_url = "https://studious-octo-adventure.firebaseio.com/"
+soundcloud = []
+youtube = []
 
 def connect():
     user_agent      = "Reddit Music Player v0.1 alpha by /u/banglatoker"
@@ -10,16 +14,34 @@ def connect():
     return r
 
 def main():
+    arg_len = len(sys.argv)
+    if arg_len < 2:
+        sub = "trap"
+    elif arg_len >= 2:
+        sub = sys.argv[1]
     r = connect()
-    subredditLinks = loadSubReddit(r)
-    getLinks(subredditLinks)
+    subredditLinks = loadSubReddit(sub, r)
+    getLinks(subredditLinks, sub)
 
-def loadSubReddit(r):
-    sub = "trap"
-    return r.subreddit(sub).stream.submissions()
+def loadSubReddit(sub, r):
+    return r.subreddit(sub).hot()
 
-def getLinks(subredditLinks):
+def getLinks(subredditLinks, sub):
     for links in subredditLinks:
-        print(links.url)
+        if "soundcloud.com" in links.url:
+            data = { 'link' : links.url, 'score': links.score, 'up': links.ups, 'down': links.downs}
+            linkType = "soundcloud"
+            postToFirebase(data, linkType, sub)
+        if "youtube.com" in links.url:
+            data = { 'link' : links.url, 'score': links.score, 'up' : links.ups, 'down': links.downs}
+            linkType = "youtube"
+            postToFirebase(data, linkType, sub)
+
+def postToFirebase(data, linkType, sub):
+    if "soundcloud" is linkType:
+        result = requests.post(firebase_url + "/" + sub + '/soundcloud.json', data=json.dumps(data))
+    if "youtube" is linkType:
+        result = requests.post(firebase_url + "/" + sub + '/youtube.json', data=json.dumps(data))
+    print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
 
 main()
